e8b88c133e350e6218dfec98365bfa96f38dc807
zhexiao <zhexiao@163.com>
2016-05-03 09:58:54 -0400

    add

--- code_update.py
+++ code_update.py
@@ -8,19 +8,19 @@ key_file = '~/.ssh/zocvpc.pem'
 
 def test():
     env.environment = "test"
-    env.hosts = ['52.5.144.54']
+    env.hosts = ['ip']
     env.user = 'ubuntu'
     env.key_filename = key_file
 
 def onedot():
     env.environment = "onedot"
-    env.hosts = ['ec2-52-20-210-122.compute-1.amazonaws.com']
+    env.hosts = ['ip']
     env.user = 'ubuntu'
     env.key_filename = key_file
 
 def live():
     env.environment = "live"
-    env.hosts = ['54.208.83.166', '52.90.50.45']
+    env.hosts = ['ip', 'ip']
     env.user = 'ubuntu'
     env.key_filename = key_file
 
